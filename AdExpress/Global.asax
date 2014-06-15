a709323ebda1cc0c537866e8738af1a1119b9398
Jeeva Nadarajah <jeeva.ganeshan@gmail.com>
2014-06-14 22:26:47 -0400

    initial commit

---
+++ AdExpress/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AdExpress.MvcApplication" Language="C#" %>
---
+++ AdExpress/Models/Ad.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity;
+
+namespace AdExpress.Models
+{
+    public class Ad
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Version { get; set; }
+        
+    }
+    public class AdDBContext : DbContext
+    {
+        public DbSet<Ad> Ads { get; set; }
+    }
+}
